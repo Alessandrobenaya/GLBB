67d0b7e99a84ea564bdd749ea5ddac4082edb3a7
unknown <11211007@student.itk.ac.id>
2023-05-06 19:29:14 +0800

    Penambahanan tombol reset & pause

--- Glbb.py
+++ Glbb.py
@@ -37,9 +37,12 @@ input_box = pygame.Rect(SCREEN_WIDTH // 2 - 50, SCREEN_HEIGHT // 2 - 50, 100, 50
 input_text = ""
 
 reset_button = pygame.Rect(SCREEN_WIDTH - 120, SCREEN_HEIGHT - 70, 100, 50)
+pause_button = pygame.Rect(SCREEN_WIDTH - 230, SCREEN_HEIGHT - 70, 100, 50)
 
 # Loop program
 running = True
+input_v = False
+pause = False
 while running:
     # Loop event pygame
     for event in pygame.event.get():
@@ -50,10 +53,10 @@ while running:
             if event.key == pygame.K_RETURN:
                 try:
                     a = float(input_text)
+                    input_v = True
                 except ValueError:
                     pass
                 input_text = ""
-
             elif event.key == pygame.K_BACKSPACE:
                 input_text = input_text[:-1]
             else:
@@ -66,7 +69,9 @@ while running:
                 a = 0
                 t = 0
                 ball_pos = (x, SCREEN_HEIGHT // 2)
-    
+                input_v = False
+            elif pause_button.collidepoint(event.pos):
+                pause = not pause
 
     # Menggambar latar belakang dengan warna putih
     screen.fill(WHITE)
@@ -75,42 +80,42 @@ while running:
     pygame.draw.circle(screen, RED, ball_pos, BALL_RADIUS)
 
     # Memperbarui posisi bola dan waktu
-    x = x + v * delta_t + 0.5 * a * delta_t ** 2
-    v = v + a * delta_t
-    ball_pos = (int(x), SCREEN_HEIGHT // 2)
+    if input_v and not pause:
+        x = x + v * delta_t + 0.5 * a * delta_t ** 2
+        v = v + a * delta_t
+        ball_pos = (int(x), SCREEN_HEIGHT // 2)
 
-    # Jika bola mencapai batas layar, balik arah
-    if ball_pos[0] + BALL_RADIUS >= SCREEN_WIDTH or ball_pos[0] - BALL_RADIUS <= 0:
-        v = -v
+        # Jika bola mencapai batas layar, balik arah
+        if ball_pos[0] + BALL_RADIUS >= SCREEN_WIDTH or ball_pos[0] - BALL_RADIUS <= 0:
+            v = -v
+
+        # Memperbarui waktu
+        t += delta_t
 
     # Menampilkan waktu pada layar
     time_text = font.render("Waktu: {:.2f} s".format(t), True, BLACK)
     screen.blit(time_text, (10, 10))
 
-    # Menampilkan kecepatan pada layar
-    speed_text = font.render("Kecepatan: {:.2f} m/s".format(v), True, BLACK)
-    screen.blit(speed_text, (10, 50))
+    # Menampilkan kecepatan
+    vel_text = vel_text = font.render("Kecepatan: {:.2f} s".format(v), True, BLACK)
+    screen.blit(vel_text, (10, 40))
+
+    accel_text = font.render("Percepatan: {:.2f} m/s^2".format(a), True, BLACK)
+    screen.blit(accel_text, (10, 70))
 
     # Menampilkan input percepatan pada layar
+    input_box_text = font.render("Input percepatan:", True, BLACK)
+    screen.blit(input_box_text, (SCREEN_WIDTH // 70 , SCREEN_HEIGHT // 2 - 200))
     input_text_surface = font.render(input_text, True, BLACK)
-    screen.blit(input_text_surface, (SCREEN_WIDTH // 2 - 40, SCREEN_HEIGHT // 2 - 40))
-    input_box.w = max(100, input_text_surface.get_width() + 10)
-    pygame.draw.rect(screen, BLACK, input_box, 2)
+    screen.blit(input_text_surface, (230, 100))
 
-    # Menampilkan Input Reset
+# Menampilkan tombol reset pada layar
     reset_text = font.render("Reset", True, BLACK)
-    pygame.draw.rect(screen, BLACK, reset_button, 2)
-    screen.blit(reset_text, (SCREEN_WIDTH - 100, SCREEN_HEIGHT - 55))
+    screen.blit(reset_text, (SCREEN_WIDTH - 100, SCREEN_HEIGHT - 50))
 
-    # Menampilkan percepatan pada layar
-    acceleration_text = font.render("Percepatan: {:.2f} m/s^2".format(a), True, BLACK)
-    screen.blit(acceleration_text, (10, 90))
+# Menampilkan tombol pause pada layar
+    pause_text = font.render("Pause", True, BLACK)
+    screen.blit(pause_text, (SCREEN_WIDTH - 200, SCREEN_HEIGHT - 50))
 
-    # Memperbarui waktu
-    t += delta_t
-
-    # Update tampilan layar
+# Update layar
     pygame.display.flip()
-
-# Quit pygame
-pygame.quit()
